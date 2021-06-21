4944224c2220e65b17ec8b06f9a7e83c53a1e3e6
Djigui <djigui@1F-01.edu-il.it>
2021-06-21 18:02:16 +0200

    done

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -3,3 +3,6 @@
 // .forEach((fruit, index) => {
     // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
 // })
+const fruites = ["bannana", "mongo", "apple",]
+fruites.forEach(fruit  => console.log(fruit))
+fruites.forEach((fruit,index)  => console.log(index,fruit))
