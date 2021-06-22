a146da5494bbb414b5af40629285b3fe944f81c0
Djigui <djigui@1F-01.edu-il.it>
2021-06-22 18:00:29 +0200

    seconda esercizio fatto

--- 01-for-each/for_each.js
+++ 01-for-each/for_each.js
@@ -3,6 +3,12 @@
 // .forEach((fruit, index) => {
     // console.log() a message interpolating the name of the fruit and it's position in the array (index increased by 1);
 // })
-const fruites = ["bannana", "mongo", "apple",]
-fruites.forEach(fruit  => console.log(fruit))
-fruites.forEach((fruit,index)  => console.log(index,fruit))
+//const fruites = ["bannana", "mongo", "apple",]
+//
+//fruites.forEach(fruit  => console.log(fruit))
+
+
+//fruites.forEach((fruit,index)  => console.log(index,fruit))
+const animali = ["muca", "pecora", "capra",];
+let map1 = animali.map(' ');
+  console.log(map1);
